676c51a0e03429ca115411ec54b4d29d7cfb6871
Oğuzhan Deniz <oguzhan506deniz@gmail.com>
2023-04-08 21:05:59 +0300

    Update EngelController.cs

--- GameJamo/Assets/Scripts/EngelController.cs
+++ GameJamo/Assets/Scripts/EngelController.cs
@@ -16,32 +16,34 @@ public class EngelController : MonoBehaviour
     {
         Enemy enemyScript = collision.GetComponent<Enemy>();
 
-        if (enemyScript.enemySO.enemyType == EnemySO.EnemyTypes.Sinirli)
+        switch (enemyScript.enemySO.enemyType)
         {
-            energy -= 10;
-            Debug.Log("Ekran sallama kodu");
-        }
-        if (enemyScript.enemySO.enemyType == EnemySO.EnemyTypes.Deli)
-        {
-            energy -= 10;
-            // Deli karakterin yön tuşlarını tersine çevirir
-            playerMovement.invert = true;
-        }
-        if (enemyScript.enemySO.enemyType == EnemySO.EnemyTypes.Uykulu)
-        {
-            energy -= 10;
-            // Uykulu karakterin hızını yarıya düşürür
-            playerMovement.moveSpeed /= 2f;
-        }
-        if (enemyScript.enemySO.enemyType == EnemySO.EnemyTypes.Depresif)
-        {
-            energy -= 50;
-            Debug.Log("Enerjiyi yariya dusurur / 50 puan duser");
-        }
-        if (enemyScript.enemySO.enemyType == EnemySO.EnemyTypes.Mutsuz)
-        {
-            energy -= 10;
-            Debug.Log("Ates etme hizi dusecek");
+            case EnemySO.EnemyTypes.Sinirli:
+                energy -= 10;
+                Debug.Log("Ekran sallama kodu");
+                break;
+
+            case EnemySO.EnemyTypes.Deli:
+                energy -= 10;
+                // Deli karakterin yön tuşlarını tersine çevirir
+                playerMovement.invert = true;
+                break;
+
+            case EnemySO.EnemyTypes.Uykulu:
+                energy -= 10;
+                // Uykulu karakterin hızını yarıya düşürür
+                playerMovement.moveSpeed /= 2f;
+                break;
+
+            case EnemySO.EnemyTypes.Depresif:
+                energy -= 50;
+                Debug.Log("Enerjiyi yariya dusurur / 50 puan duser");
+                break;
+
+            case EnemySO.EnemyTypes.Mutsuz:
+                energy -= 10;
+                Debug.Log("Ates etme hizi dusecek");
+                break;
         }
     }
 }
