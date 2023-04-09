a0828f9cf1c0c65ae2b4c557ef799bebf68a6dc5
Tahir1072a <tahirifdn@gmail.com>
2023-04-09 13:42:32 +0300

    Ateş etme tamamlandı

---
+++ GameJamo/Assets/Scripts/BulletControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BulletControl : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
